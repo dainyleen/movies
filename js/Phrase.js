404ac55653a7e6a555635a2b99360768864fe232
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-05 17:37:50 +1200

    Added addPhraseToDisplay method on Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,4 +3,16 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase()
   }
+
+  /**
+  * Display phrase on game board
+  */
+
+  addPhraseToDisplay() {
+    const wordPhrases = this.phrase.split(' ')
+    const ul = document.createElement('ul')
+    const div = document.getElementById('phrase')
+
+    
+  }
 }
