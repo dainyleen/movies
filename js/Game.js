c7ae9f9608a7790ee4084bc95fc07ba5eb294e6e
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-05 17:19:29 +1200

    Created createPhrases method inside the Game class

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,26 @@
 // Create initial constructor method
 class Game {
   constructor(missed, phrases, activePhrase) {
-    this.missed = missed
-    this.phrases = phrases
-    this.activePhrase = activePhrase
+    this.missed = 0
+    this.phrases = this.createPhrases()
+    this.activePhrase = null
+  }
+
+  /**
+  * Creates phrases for use in game
+  * @return {array} An array of phrases that could be used in the game
+  */
+
+   
+  createPhrases() {
+    const phrases = [
+      new Phrase('Life is like a box of chocolates'),
+      new Phrase('I am your father'),
+      new Phrase('Houston, we have a problem'),
+      new Phrase('You had me at hello'),
+      new Phrase('Hasta la vista, baby')
+    ]
+
+    return phrases
   }
 }
--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 //Defining variables
-const missed = 0
-const phrase = [
-  new Phrase('Life is like a box of chocolates'),
-  new Phrase('I am your father'),
-  new Phrase('Houston, we have a problem'),
-  new Phrase('You had me at hello'),
-  new Phrase('Hasta la vista, baby')
-]
-const activePhrase = null
+const phrase = new Phrase('Life is like a box of chocolates');
 
+console.log(`Phrase - phrase: ${phrase.phrase}`);
 
+const game = new Game()
+
+game.phrases.forEach((phrase, index) => {
+  console.log(`Phrase ${index} - phrase: ${phrase.phrase}`)
+})
