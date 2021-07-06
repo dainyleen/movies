0a4e0f21a04b4e219c1a506e1504154a71d53c59
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-06 19:21:39 +1200

    Re-structured class Game properties

--- js/Game.js
+++ js/Game.js
@@ -2,8 +2,8 @@
 class Game {
   constructor(missed, phrases, activePhrase) {
     this.missed = missed
-    this.phrases = this.createPhrases()
-    this.activePhrase = null
+    this.phrases = phrases
+    this.activePhrase = activePhrase
   }
 
   /**
@@ -11,17 +11,7 @@ class Game {
   * @return {array} An array of phrases that could be used in the game
   */
  
-  createPhrases() {
-    const phrases = [
-      new Phrase('Life is like a box of chocolates'),
-      new Phrase('I am your father'),
-      new Phrase('Houston, we have a problem'),
-      new Phrase('You had me at hello'),
-      new Phrase('Hasta la vista, baby')
-    ]
-
-    return phrases
-  }
+  
 
   /**
   * Selects random phrase from phrases property
